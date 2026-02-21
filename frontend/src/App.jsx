110b57baa68aa2322fe5ecad92e042037e91e1db
ROYGBPJ <83476475+ROYGBPJ@users.noreply.github.com>
2026-02-21 15:44:48 -0700

    Add boilerplate App component for React application entry point

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to the Video to Roblox R15 App!</h1>
+        </div>
+    );
+};
+
+export default App;
